300cf25bc5c04f7f0d5fd40ce525aafd2c43b4a1
aldonajudeson <aldonajudeson@gmail.com>
2025-08-07 13:15:32 +0530

    initial

--- form.js
+++ form.js
@@ -1,5 +1,11 @@
+let heading=document.getElementById("abcd");
+heading.addEventListener('click',()=>{console.log("add event listener");
+})
+
 function a(e)
 {
     e.preventDefault()
     console.log(e.target.value);
 }
+
+localStorage.setItem('admin',123)
